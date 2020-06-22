fadd13f02fce10242515a2cecddcfc820aaebb9c
Pierre-Yves Aillet <pyaillet@gmail.com>
2020-06-22 19:20:22 +0200

    :sparkles: handle /

--- server/server.go
+++ server/server.go
@@ -9,6 +9,28 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 )
 
+const usage = `<html><head><title>Not found</title></head><body>
+<h1>Not found</h1>
+
+<p>
+Give a username and get an avatar in return: <a href="https://avatars.charlyx.dev/twitter?username=charlyx">https://avatars.charlyx.dev/twitter?username=charlyx</a>
+</p>
+
+<p>
+You can ask for variant sizings such as "bigger", "mini" and "original" (default size being "normal").
+</p>
+
+<p>
+Example : <a href="https://avatars.charlyx.dev/twitter?username=charlyx&size=bigger">https://avatars.charlyx.dev/twitter?username=charlyx&size=bigger</a>
+</p>
+</body>
+</html>`
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, usage)
+}
+
 func Start(port string) error {
 	if port == "" {
 		port = "8080"
@@ -25,6 +47,7 @@ func Start(port string) error {
 	}
 
 	http.HandleFunc("/twitter", twitter.Handler(twitterToken, cache))
+	http.HandleFunc("/", defaultHandler)
 
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
