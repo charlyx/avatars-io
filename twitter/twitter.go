398be4d8b4d06ce6eea32e10831c809f51f0adda
Charles-Henri GUÉRIN <charleshenri.guerin@icloud.com>
2020-06-22 19:53:22 +0200

    :sparkles: Add default image
    Answers with default image when specified user does not exist or an error occurs.

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -16,12 +16,15 @@ type profile struct {
 	ImageURL string `json:"profile_image_url"`
 }
 
-func getUserProfileImageURL(username, token string) (string, error) {
+func getUserProfileImageURL(username, token string) string {
+	const defaultImageURL = "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
+
 	userShowURL := fmt.Sprintf("%s/1.1/users/show.json?screen_name=%s", twitterAPI, username)
 
 	req, err := http.NewRequest("GET", userShowURL, nil)
 	if err != nil {
-		return "", err
+		log.Printf("got an error creating GET request for %s", userShowURL)
+		return defaultImageURL
 	}
 
 	authorization := fmt.Sprintf("Bearer %s", token)
@@ -30,16 +33,22 @@ func getUserProfileImageURL(username, token string) (string, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		log.Printf("got an error requesting profile image URL for user %s", username)
+		return defaultImageURL
 	}
 	defer resp.Body.Close()
 
 	userProfile := &profile{}
 	if err := json.NewDecoder(resp.Body).Decode(&userProfile); err != nil {
-		return "", err
+		log.Printf("got an error decoding user %s profile", username)
+		return defaultImageURL
 	}
 
-	return userProfile.ImageURL, nil
+	if userProfile.ImageURL == "" {
+		return defaultImageURL
+	}
+
+	return userProfile.ImageURL
 }
 
 func getNormalizedSize(size string) string {
@@ -69,7 +78,7 @@ func Handler(token string, cache simplelru.LRUCache) func(http.ResponseWriter, *
 
 		if username == "" {
 			log.Print("no username given")
-			http.NotFound(w, req)
+			http.Error(w, "You must specify username query parameter.", http.StatusBadRequest)
 			return
 		}
 
@@ -88,20 +97,15 @@ func Handler(token string, cache simplelru.LRUCache) func(http.ResponseWriter, *
 		}
 
 		if profileImageURL == "" {
-			newProfileImageURL, err := getUserProfileImageURL(username, token)
-			if err != nil {
-				http.NotFound(w, req)
-				log.Printf("failed getting profile image URL for user %s: %s", username, err.Error())
-				return
-			}
+			profileImageURL = getUserProfileImageURL(username, token)
+
+			log.Printf("got profile image URL %s for user %s", profileImageURL, username)
 
 			if cache != nil {
-				cache.Add(username, newProfileImageURL)
-				log.Printf("cached URL %s for user %s", newProfileImageURL, username)
-			}
+				cache.Add(username, profileImageURL)
 
-			profileImageURL = newProfileImageURL
-			log.Printf("got profile image URL %s for user %s", profileImageURL, username)
+				log.Printf("cached URL %s for user %s", profileImageURL, username)
+			}
 		}
 
 		sizedProfileImageURL := getSizedProfileImageURL(profileImageURL, size)
