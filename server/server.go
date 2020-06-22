9f19630bcbf447d8ff3a600454547598683f91ae
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2020-06-22 21:10:42 +0200

    :sparkles: Add default handler explaining usage (#2)
    * :sparkles: handle /
    
    * :ok_hand: fix typo
    
    Co-authored-by: Charles-Henri GUERIN <charleshenri.guerin@me.com>
    
    * :ok_hand: add favicon
    
    Co-authored-by: Charles-Henri GUERIN <charleshenri.guerin@me.com>

--- server/server.go
+++ server/server.go
@@ -2,12 +2,35 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/charlyx/avatars.io/secrets"
 	"github.com/charlyx/avatars.io/twitter"
 	lru "github.com/hashicorp/golang-lru"
 )
+
+const usage = `<html><head><title>Not found</title></head><link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👤</text></svg>"><body>
+<h1>Not found</h1>
+
+<p>
+Give a username and get an avatar in return: <a href="https://avatars.charlyx.dev/twitter?username=charlyx">https://avatars.charlyx.dev/twitter?username=charlyx</a>
+</p>
+
+<p>
+You can ask for variant sizings such as "bigger", "mini" and "original" (default size being "normal").
+</p>
+
+<p>
+Example: <a href="https://avatars.charlyx.dev/twitter?username=charlyx&size=bigger">https://avatars.charlyx.dev/twitter?username=charlyx&size=bigger</a>
+</p>
+</body>
+</html>`
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	io.WriteString(w, usage)
+}
 
 func Start(port string) error {
 	if port == "" {
@@ -25,6 +48,7 @@ func Start(port string) error {
 	}
 
 	http.HandleFunc("/twitter", twitter.Handler(twitterToken, cache))
+	http.HandleFunc("/", defaultHandler)
 
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
